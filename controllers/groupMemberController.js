72deae32148fe7651fb3d767847e8636653e1e7e
Maryamabdi52 <maryamabdimohamed63@gmail.com>
2025-06-04 03:20:50 +0700

    Update

--- controllers/groupMemberController.js
+++ controllers/groupMemberController.js
@@ -1,5 +1,6 @@
 const GroupMember = require("../models/GroupMember");
 
+// Add a new member
 exports.addGroupMember = async (req, res) => {
   try {
     const member = new GroupMember(req.body);
@@ -10,6 +11,7 @@ exports.addGroupMember = async (req, res) => {
   }
 };
 
+// Get members by groupId
 exports.getGroupMembers = async (req, res) => {
   try {
     const members = await GroupMember.find({ group_id: req.params.groupId })
@@ -20,6 +22,32 @@ exports.getGroupMembers = async (req, res) => {
   }
 };
 
+// Get all members (no group filter)
+exports.getAllGroupMembers = async (req, res) => {
+  try {
+    const members = await GroupMember.find().populate("user_id", "full_name email");
+    res.status(200).json(members);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update a group member by ID
+exports.updateGroupMember = async (req, res) => {
+  try {
+    const member = await GroupMember.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!member) return res.status(404).json({ error: "Member not found" });
+    res.status(200).json(member);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Remove a member
 exports.removeGroupMember = async (req, res) => {
   try {
     const removed = await GroupMember.findByIdAndDelete(req.params.id);
