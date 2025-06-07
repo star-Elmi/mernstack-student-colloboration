e660b1d1fb5fa85cea0f4bd9592599439c97b91c
Maymun <maymu@gmail.com>
2025-06-07 10:13:54 +0300

    added discuss control

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,1 +1,9 @@
 // fileRoutes.js
+const express = require('express');
+const router = express.Router();
+const fileController = require('../controllers/fileController');
+
+router.post('/', fileController.uploadFile);
+router.get('/', fileController.getAllFiles);
+
+module.exports = router;
